6d51cdbc449b1d655a296c3dac9ae0b6b590db0a
lake28 <jrdnlake@gmail.com>
2020-04-13 20:14:20 +0100

    added function for both converts

--- Convert-Temp.ps1
+++ Convert-Temp.ps1
@@ -5,19 +5,36 @@
 
 # To help you along, you can use these formulas to convert temperatures. To convert from Celsius to Fahrenheit: (C*1.8)+32. To convert from Fahrenheit to Celsius is a matter of reversing the process: (F-32)/1.8. As points of reference, freezing is 32F and 0C. Boiling is 212F and 100C.
 
-function ConvertTo-Celsius {
+function ConvertTo-Fahrenheit {
     [CmdletBinding()]
     param (
-        $temparture        
+        $Temperature
+               
     )
 
-    $ConvertedTemp = ($temparture * 1.8) + 32
+    $ConvertedTemp = ($Temperature * 1.8) + 32
 
     $TempObject = [PSCustomObject]@{
         "Temparture in Fahrenheit" = $ConvertedTemp
-        "Temparture in Celsius"    = $ConvertedTemp
+        "Temparture in Celsius"    = $Temperature
     }
     Write-Output $TempObject
     
 }
 
+
+function ConvertTo-Celsius {
+    [CmdletBinding()]
+    param (
+        $Temperature       
+    )
+
+    $ConvertedTemp = ($Temperature - 32) /1.8
+
+    $TempObject = [PSCustomObject]@{
+        "Temperature in Fahrenheit" = $Temperature
+        "Temperature in Celsius"    = $ConvertedTemp
+    }
+    Write-Output $TempObject
+    
+}
